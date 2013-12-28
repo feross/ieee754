7c780a1d43ae7f12d0607a9a0a91751a09ded3e4
Feross Aboukhadijeh <feross@feross.org>
2013-12-28 01:10:12 -0800

    make test clearer

--- test/basic.js
+++ test/basic.js
@@ -4,8 +4,8 @@ var test = require('tape')
 var EPSILON = 0.00001
 
 test('read float', function (t) {
-  var buf = new Buffer('14ae2942', 'hex')
-
+  var buf = new Buffer(4)
+  buf.writeFloatLE(42.42, 0)
   var num = ieee754.read(buf, 0, true, 23, 4)
   t.ok(Math.abs(num - 42.42) < EPSILON)
 
